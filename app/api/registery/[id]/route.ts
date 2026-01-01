673db4187a781ceb47cb06d808c7977da5eee2b2
Parashar Das <87603143+porashor@users.noreply.github.com>
2026-01-01 23:53:15 +0600

    object id make

--- app/api/registery/[id]/route.ts
+++ app/api/registery/[id]/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server"; 
 import UserModel from '@/app/api/schema/userSchema'
 import {client} from "@/app/api/lib/mongodb";
-
+import mongoose from "mongoose";
 
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
     try {
         const { id } = await params;
         await client();
-        const posts = await UserModel.find({ _id: id });
+        const objectId = new mongoose.Types.ObjectId(id);
+        const posts = await UserModel.find({ _id: objectId });
         const {_id, name, profilePic} = posts[0]
         const sendableData = {_id, name, profilePic}
         console.log("posts", sendableData);
